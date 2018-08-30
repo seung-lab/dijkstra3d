31fbe9f77f323992f7a4f0c297d49d5c8dd85775
William Silversmith <william.silversmith@gmail.com>
2018-08-30 19:40:21 -0400

    test: added 3d tests

--- automated_test.py
+++ automated_test.py
@@ -2,74 +2,93 @@ import dijkstra
 import numpy as np
 
 def test_dijkstra2d_10x10():
-  values = np.ones((10,10,1), dtype=np.float32)
-  
-  path = dijkstra.dijkstra(values, (0,0,0), (3,0,0))
+  for dtype in (np.int32, np.float32, np.float64):
+    values = np.ones((10,10,1), dtype=dtype)
+    
+    path = dijkstra.dijkstra(values, (0,0,0), (3,0,0))
 
-  assert len(path) == 4
-  assert np.all(path == np.array([
-    [0,0,0],
-    [1,1,0],
-    [2,1,0],
-    [3,0,0],
-  ]))
+    assert len(path) == 4
+    assert np.all(path == np.array([
+      [0,0,0],
+      [1,1,0],
+      [2,1,0],
+      [3,0,0],
+    ]))
 
-  path = dijkstra.dijkstra(values, (0,0,0), (5,5,0))
+    path = dijkstra.dijkstra(values, (0,0,0), (5,5,0))
 
-  assert len(path) == 6
-  assert np.all(path == np.array([
-    [0,0,0],
-    [1,1,0],
-    [2,2,0],
-    [3,3,0],
-    [4,4,0],
-    [5,5,0],
-  ]))
+    assert len(path) == 6
+    assert np.all(path == np.array([
+      [0,0,0],
+      [1,1,0],
+      [2,2,0],
+      [3,3,0],
+      [4,4,0],
+      [5,5,0],
+    ]))
 
-  path = dijkstra.dijkstra(values, (0,0,0), (9,9,0))
-  
-  assert len(path) == 10
-  assert np.all(path == np.array([
-    [0,0,0],
-    [1,1,0],
-    [2,2,0],
-    [3,3,0],
-    [4,4,0],
-    [5,5,0],
-    [6,6,0],
-    [7,7,0],
-    [8,8,0],
-    [9,9,0]
-  ]))
+    path = dijkstra.dijkstra(values, (0,0,0), (9,9,0))
+    
+    assert len(path) == 10
+    assert np.all(path == np.array([
+      [0,0,0],
+      [1,1,0],
+      [2,2,0],
+      [3,3,0],
+      [4,4,0],
+      [5,5,0],
+      [6,6,0],
+      [7,7,0],
+      [8,8,0],
+      [9,9,0]
+    ]))
 
 def test_dijkstra2d_10x10_off_origin():
-  values = np.ones((10,10,1), dtype=np.float32)
-  
-  path = dijkstra.dijkstra(values, (2,0,0), (3,0,0))
+  for dtype in (np.int32, np.float32, np.float64):
+    values = np.ones((10,10,1), dtype=dtype)
+    
+    path = dijkstra.dijkstra(values, (2,0,0), (3,0,0))
 
-  assert len(path) == 2
+    assert len(path) == 2
+    assert np.all(path == np.array([
+      [2,0,0],
+      [3,0,0],
+    ]))
+
+    path = dijkstra.dijkstra(values, (2,1,0), (3,0,0))
+
+    assert len(path) == 2
+    assert np.all(path == np.array([
+      [2,1,0],
+      [3,0,0],
+    ]))
+
+    path = dijkstra.dijkstra(values, (9,9,0), (5,5,0))
+
+    assert len(path) == 5
+    assert np.all(path == np.array([
+      [9,9,0],
+      [8,8,0],
+      [7,7,0],
+      [6,6,0],
+      [5,5,0],
+    ]))
+
+def test_dijkstra3d_3x3x3():
+  values = np.ones((3,3,3), dtype=np.float32)
+
+  path = dijkstra.dijkstra(values, (0,0,0), (2,2,2))
   assert np.all(path == np.array([
-    [2,0,0],
-    [3,0,0],
+    [0,0,0],
+    [1,1,1],
+    [2,2,2]
   ]))
 
-  path = dijkstra.dijkstra(values, (2,1,0), (3,0,0))
-
-  assert len(path) == 2
+  path = dijkstra.dijkstra(values, (2,2,2), (0,0,0))
   assert np.all(path == np.array([
-    [2,1,0],
-    [3,0,0],
-  ]))
-
-  path = dijkstra.dijkstra(values, (9,9,0), (5,5,0))
-
-  assert len(path) == 5
-  assert np.all(path == np.array([
-    [9,9,0],
-    [8,8,0],
-    [7,7,0],
-    [6,6,0],
-    [5,5,0],
+    [2,2,2],
+    [1,1,1],
+    [0,0,0]
   ]))
 
 def test_distance_field_2d():
