c5f74dcf2c5d73573e7c8a841aad24100d34724f
William Silversmith <william.silversmith@gmail.com>
2018-11-27 18:26:08 -0500

    test: fill out cpp test code with an example

--- test.cpp
+++ test.cpp
@@ -1,8 +1,18 @@
 #include "dijkstra3d.hpp"
 #include <cstdint>
+#include <cstdio>
 
 int main () {
-  uint8_t* labels = new uint8_t[512*512*512];
+  const int dim = 256;
+  const int voxels = dim * dim * dim;
+  uint8_t* labels = new uint8_t[voxels];
+  for (int i = 0; i < voxels; i++) {
+    labels[i] = 1;
+  }
 
+  uint32_t* x = dijkstra::parental_field3d<uint8_t>(labels, dim, dim, dim, 0);
 
+  printf("\n%d\n", x[0]);
+
+  return 1;
 }
