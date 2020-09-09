68bf5fbd8966ea78ea085a83ab993cb8a5e98c49
William Silversmith <william.silversmith@gmail.com>
2020-09-09 00:04:32 -0400

    chore(build): add infrastructure for building manylinux2010 & 2014

---
+++ build_linux.sh
@@ -0,0 +1,7 @@
+#!/usr/local/bin/bash
+# docker build . -f manylinux1.Dockerfile --tag seunglab/dijkstra3d:manylinux1
+# docker build . -f manylinux2010.Dockerfile --tag seunglab/dijkstra3d:manylinux2010
+docker build . -f manylinux2014.Dockerfile --tag seunglab/dijkstra3d:manylinux2014
+# docker run -v $PWD/dist:/output seunglab/dijkstra3d:manylinux1 /bin/bash -c "cp -r wheelhouse/* /output"
+# docker run -v $PWD/dist:/output seunglab/dijkstra3d:manylinux2010 /bin/bash -c "cp -r wheelhouse/* /output"
+docker run -v $PWD/dist:/output seunglab/dijkstra3d:manylinux2014 /bin/bash -c "cp -r wheelhouse/* /output"
