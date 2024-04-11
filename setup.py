1ad6adc428a65952ca8dd979c282999ccf722708
William Silversmith <william.silversmith@gmail.com>
2024-04-11 17:16:23 -0400

    release(1.15.0): adds feature map argument to edf

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if sys.platform == 'darwin':
 
 setuptools.setup(
   name="dijkstra3d",
-  version="1.14.0",
+  version="1.15.0",
   python_requires=">=3.7,<4.0",
   setup_requires=['numpy','cython'],
   ext_modules=[
@@ -54,11 +54,11 @@ setuptools.setup(
     "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
     "Programming Language :: Python",
     "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
     "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12",
     "Topic :: Scientific/Engineering",
     "Intended Audience :: Science/Research",
     "Operating System :: POSIX",
